335df6873fc5b1107e86501a0baf0efc54367ee6
ShigaRy <honki237@gmail.com>
2025-08-06 21:34:26 +0900

    自己紹介

---
+++ HelloWorld_ShigaRyosei/src/sample/selfintro.java
@@ -0,0 +1,18 @@
+package sample;
+
+public class selfintro {
+
+	public static void main(String[] args) {
+		String name = "志賀 伶聖";	//名前
+		int age = 20;	//年齢
+		String hobby = "ゲーム";	//趣味
+		String comment = "ちょっとだけプログラミングしたことあります";	//一言
+		
+		//以下出力
+		System.out.println("私の名前は" + name + "です。");
+		System.out.println("年齢は" + age + "歳です。");
+		System.out.println("趣味は" + hobby + "です。");
+		System.out.println("一言：" +  comment);
+	}
+
+}

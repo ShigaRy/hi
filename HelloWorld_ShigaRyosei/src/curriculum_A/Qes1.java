14e7255c334330cb4c7eb620a4674d48e7a3b5fe
ShigaRy <honki237@gmail.com>
2025-08-13 23:13:32 +0900

    更新 2025/8/13

--- HelloWorld_ShigaRyosei/src/curriculum_A/Qes1.java
+++ HelloWorld_ShigaRyosei/src/curriculum_A/Qes1.java
@@ -1,45 +1,57 @@
 package curriculum_A;
 
 public class Qes1 {
-	//1
-	static int intNum;					//intNumをintで宣言
-	static long longNum;				//longNumをlongで宣言
-	static float floatNum;				//floatNumをfloatで宣言
-	static double doubleNum; 			//doubleNumをdoubleで宣言
-	static char letter;				//letterをcharで宣言
-	static String letters;				//lettersをStringで宣言
-	static boolean isBoolean;			//isBooleanをBooleanで宣言
 	
-	static String name = "山田太郎";	//nameをStringで宣言、名前を代入 問題4
-	
-	static int age = 25;				//ageをintで宣言、年齢を代入 問題5
-	
-	static int num1 = 10;				//num1をintで宣言、10を代入 問題6
-	static int num2 = 5;				//num2をintで宣言、5を代入 問題6
-	static int sum = num1 + num2;		//sumをintで宣言、num1とnum2を足した数を代入 問題6
-	
-	static int score = 80;				//scoreをintで宣言、点数を代入 問題7
-	
-	static double prise = 99.99;		//priseをdoubleで宣言、値段を代入 問題8
-	
-	static String numStr = "123";		//numStrをStringで宣言、文字列を代入 問題9
-	
-	static int num = 50;				//numをintで宣言、整数を代入 問題10
-	
-	static int a = 10;					//aをintで宣言、整数を代入 問題11
-	static int b = 20;					//bをintで宣言、整数を代入 問題11
-	static boolean result;				//resultをbooleanで宣言 問題11
-	
-	static int x = 15;					//xをintで宣言 問題12
-	
-	static String language;			//languageをStringで宣言 問題13
-	
-	//2
 	public static void main(String[] args) {
-		Byte byteNum;				//byteNumをByteで宣言
-		Short shortNum;				//shortNumをShortで宣言
+		//1
+		byte byteNum = 0;				//byteNumをByteで宣言
+		Short shortNum = 0;	//shortNumをShortで宣言
 		byteNum = 10; 				//byteNumに10を代入
 		shortNum = 100;				//shortNumに100を代入
+		int intNum = 0;					//intNumをintで宣言&初期値設定
+		long longNum = 0;				//longNumをlongで宣言&初期値設定
+		float floatNum = 0.0f;				//floatNumをfloatで宣言&初期値設定
+		double doubleNum = 0.0; 			//doubleNumをdoubleで宣言&初期値設定
+		char letter = 0;				//letterをcharで宣言&初期値設定
+		String letters = null;				//lettersをStringで宣言&初期値設定
+		boolean isBoolean = false;			//isBooleanをBooleanで宣言&初期値設定
+		
+		//2
+		byteNum = 10; 				//byteNumに10を代入
+		shortNum = 100;				//shortNumに100を代入
+		intNum = 1000;				//intNumに1000を代入
+		longNum = 10000;			//longNumに10000を代入
+		floatNum = 9.5f; 			//floatNumに9.5fを代入
+		doubleNum = 10.5;			//doubleNumに10.5を代入
+		letter = 'a';				//letterにaを代入
+		letters = "ハロー";		//lettersにハローを代入
+		isBoolean = true;			//isBooleanをtrueに変更
+		
+		
+		String name = "山田太郎";	//nameをStringで宣言、名前を代入 問題4
+		
+		int age = 25;				//ageをintで宣言、年齢を代入 問題5
+		
+		int num1 = 10;				//num1をintで宣言、10を代入 問題6
+		int num2 = 5;				//num2をintで宣言、5を代入 問題6
+		int sum = num1 + num2;		//sumをintで宣言、num1とnum2を足した数を代入 問題6
+		
+		int score = 80;				//scoreをintで宣言、点数を代入 問題7
+		
+		double prise = 99.99;		//priseをdoubleで宣言、値段を代入 問題8
+		
+		String numStr = "123";		//numStrをStringで宣言、文字列を代入 問題9
+		
+		int num = 50;				//numをintで宣言、整数を代入 問題10
+		
+		int a = 10;					//aをintで宣言、整数を代入 問題11
+		int b = 20;					//bをintで宣言、整数を代入 問題11
+		boolean result;				//resultをbooleanで宣言 問題11
+		
+		int x = 15;					//xをintで宣言 問題12
+		
+		String language;			//languageをStringで宣言 問題13
+		
 		intNum = 1000;				//intNumに1000を代入
 		longNum = 10000;			//longNumに10000を代入
 		floatNum = 9.5f; 			//floatNumに9.5fを代入
